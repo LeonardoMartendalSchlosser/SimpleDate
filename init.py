6e16e2d0d20564eefc4b50cd5909f1ec373e3050
LeonardoMartendalSchlosser <lmartendalschlosser@gmail.com>
2025-11-25 16:42:30 -0300

    Initialize init.py with module exports

---
+++ init.py
@@ -0,0 +1,10 @@
+from .formatter import format_date
+from .difference import days_between
+from .parser import parse_date, today
+
+__all__ = [
+    "format_date",
+    "days_between",
+    "parse_date",
+    "today",
+]
